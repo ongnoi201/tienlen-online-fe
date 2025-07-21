6c7fdb2a03bf954491001cac59a6f7e343ee4c4a
laogia201 <o0oking.dark@gmail.com>
2025-07-21 17:29:25 +0700

    up2

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const user = JSON.parse(localStorage.getItem('user') || '{}');
 const userId = user._id || user.id || null;
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://tienlen-online-be.onrender.com', {
     auth: { userId }
 });
 
